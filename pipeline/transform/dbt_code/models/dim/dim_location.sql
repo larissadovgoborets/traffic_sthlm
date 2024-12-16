f972115f5cba9f84781efd3b0bf32cd820f0edd6
simontroedsson <simon.troedsson94@gmail.com>
2024-12-16 16:37:07 +0100

    dbt replace null values

--- pipeline/transform/dbt_code/models/dim/dim_location.sql
+++ pipeline/transform/dbt_code/models/dim/dim_location.sql
@@ -3,6 +3,6 @@ WITH src_dim_location AS (SELECT * FROM {{ ref('src_location') }})
 
 SELECT
     {{ dbt_utils.generate_surrogate_key(['location_id','coordinates']) }} AS location_id,
-    location_descriptor,
+    COALESCE(location_descriptor, 'ej specificerad') AS location_descriptor,
     coordinates
 FROM src_dim_location
--- pipeline/transform/dbt_code/models/dim/dim_traffic_deviation_details.sql
+++ pipeline/transform/dbt_code/models/dim/dim_traffic_deviation_details.sql
@@ -5,12 +5,12 @@ SELECT
     {{ dbt_utils.generate_surrogate_key(['deviation_id','affected_direction']) }} AS deviation_id,
     affected_direction,
     affected_direction_value,
-    number_of_lanes_restricted,
-    road_number,
-    road_number_numeric,
-    traffic_restriction_type,
-    temporary_limit,
-    road_name,
-    severity_code,
-    severity_text
+    COALESCE(number_of_lanes_restricted, 0) AS number_of_lanes_restricted,
+    COALESCE(road_number, 'ej specificerad') AS road_number,
+    COALESCE(road_number_numeric,0) AS road_number_numeric,
+    COALESCE(traffic_restriction_type,'ej specificerad') AS traffic_restriction_type,
+    COALESCE(temporary_limit,'ej specificerad') AS temporary_limit,
+    COALESCE(road_name, 'ej specificerad') AS road_name,
+    COALESCE(severity_code,0) AS severity_code,
+    COALESCE(severity_text, 'ej specificerad') AS severity_text
 FROM src_traffic_deviation_details

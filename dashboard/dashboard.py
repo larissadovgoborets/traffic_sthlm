fcfc6b19c5159432d16318d3ce34d906888c876d
simontroedsson <simon.troedsson94@gmail.com>
2024-12-18 13:02:12 +0100

    added different tooltips depending on deviation

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -10,40 +10,62 @@ from datetime import datetime
 
 STOCKHOLM_CENTER = (59.325,18.05)
 
-
-def build_html_string(deviation,start_date,end_date)-> str:
-    if deviation == "simple":
-        html_content = f"""
-                <h4>{deviation}</h4>
-                <pre>Giltlig från:       {start_date}<br>Beräknas pågå till: {end_date}</pre>
-                """
-    elif deviation == "detailed":
-        html_content = f"""
-                <h4>{deviation}</h4>
-                <pre>Giltlig från:       {start_date}<br>Beräknas pågå till: {end_date}</pre>
-                """
-    elif deviation == "alert":
-        html_content = f"""
-                <h4>{deviation}</h4>
-                <pre>Giltlig från:       {start_date}<br>Beräknas pågå till: {end_date}</pre>
-                """
-    else:
-        html_content = f"""
-                <h4>{deviation}</h4>
-                <pre>Giltlig från:       {start_date}<br>Beräknas pågå till: {end_date}</pre>
-                """
-    return html_content
-def create_marker_tooltip(df) -> IFrame:  
-    deviation = df['MESSAGE_CODE']
+def get_date(df):
     start_date = datetime.fromisoformat(str(df['START_TIME'])).strftime("%Y-%m-%d %H:%M")
 
     if pd.isnull(df['END_TIME']):
         end_date = "tid ej angiven"
     else:
         end_date = datetime.fromisoformat(str(df['END_TIME'])).strftime("%Y-%m-%d %H:%M")
+    return (start_date,end_date)
+
+def build_html_string(df,start_date,end_date)-> str:
+    deviation = df['MESSAGE_CODE']
+    if deviation == "Färja":
+        html_content = f"""
+        <div style="width: 301px; box-sizing: border-box;">
+        <h4>{deviation}</h4>
+        <pre>Giltlig från:       {start_date}<br>Beräknas pågå till: {end_date}</pre>
+        </div>
+
+        """
+    elif deviation == "vägarbete":
+        html_content = f"""
+                <div style="width: 301px; box-sizing: border-box;">
+                    <h4>{deviation}</h4>
+                    <pre>Giltlig från:       {start_date}<br>Beräknas pågå till: {end_date}</pre>
+                </div>
+                """
+    elif deviation == "Vägen avstängd":
+        html_content = f"""
+                <div style="width: 301px; box-sizing: border-box;">
+                    <h4>{deviation}</h4>
+                    <pre>Giltlig från:       {start_date}<br>Beräknas pågå till: {end_date}</pre>
+                </div>
+                """
+    elif deviation == "Hastighetsbegränsning gäller":
+        html_content = f"""
+                <h4>{deviation}</h4>
+                <p>{df['ROAD_NUMBER']}</p>
+                <p>Tillfälliga begränsningar:<br>
+                {df['TEMPORARY_LIMIT']}<br>
+                {df['AFFECTED_DIRECTION']}
+                </p>
+                <pre>Giltlig från:       {start_date}<br>Beräknas pågå till: {end_date}</pre>
+                """
+    else:
+        html_content = f"""
+                <div style="width: 301px; box-sizing: border-box;">
+                    <h4>{deviation}</h4>
+                    <pre>Giltlig från:       {start_date}<br>Beräknas pågå till: {end_date}</pre>
+                </div>
+                """
+    return html_content
+def create_marker_tooltip(df) -> str:  
    
-    html_content = build_html_string(deviation,start_date,end_date)
-    return IFrame(html_content, width=300, height=100)  
+    start_date,end_date = get_date(df)
+    html_content = build_html_string(df,start_date,end_date)
+    return html_content 
 
 def layout():
     df = query_traffic_messages().dropna(subset=['LONGITUDE'])
@@ -71,7 +93,7 @@ def layout():
     for index, row in df.iterrows():
         location = row['LATITUDE'],row['LONGITUDE']
         deviation = row['MESSAGE_CODE']
-        iframe = create_marker_tooltip(row)
+        html_string = create_marker_tooltip(row)
         
         if deviation == "Hastighetsbegränsning gäller":
             speed_limit = row['TEMPORARY_LIMIT']
@@ -80,7 +102,7 @@ def layout():
             image_path = traffic_deviation_icons[deviation]
         folium.Marker(location,
                         icon=folium.CustomIcon(icon_image=image_path),
-                        popup=folium.Popup(iframe, max_width=300)).add_to(map)
+                        popup=folium.Popup(html_string,max_width=None)).add_to(map)
     
     st_folium(map,use_container_width=True)
 

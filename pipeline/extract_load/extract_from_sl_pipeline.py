1fdfc9fd353ff50690470f9c0dd2ff045ed175cb
Larissa Dovgoborets <larissa.dovgoborets@outlook.com>
2024-12-18 16:50:13 +0100

    update extract_from_sl_pipeline.py, just testing - not working as it should

--- pipeline/extract_load/extract_from_sl_pipeline.py
+++ pipeline/extract_load/extract_from_sl_pipeline.py
@@ -12,12 +12,36 @@ def get_sl_announcements():
     response.raise_for_status()
     return response.json()
 
+def get_station_coordinates(announcement):
+    try: 
+        station = announcement['scope']['stop_areas'][0]['name']
+        station_data = requests.get(f"https://journeyplanner.integration.sl.se/v1/typeahead.json?searchstring={station}&stationsonly=true&maxresults=1&key=TRAFIKLAB-SLAPI-INTEGRATION-2024")
+        station_data = station_data.json()
+        lon = station_data['ResponseData'][0]['X']
+        lon = int(lon) / 10**(len(lon)-2) # Convert to decimal degrees
+        lat = station_data['ResponseData'][0]['Y']
+        lat = int(lat) / 10**(len(lat)-2) # Convert to decimal degrees
+        station_coordinates = (lat, lon)    
+    except KeyError:
+        station_coordinates = None
+    return station_coordinates
+
 @dlt.resource(write_disposition="replace")
 def sl_announcements_resource():
     for announcement in get_sl_announcements():
         yield announcement
 
-def run_pipeline(table_name="sl_announcements"):
+@dlt.resource(write_disposition="replace")
+def coordinates_resource():
+    for announcement in get_sl_announcements():
+        station_coordinates = get_station_coordinates(announcement)
+        yield station_coordinates
+
+@dlt.source
+def sl_announcements_with_coordinates():
+    return sl_announcements_resource(), coordinates_resource()
+
+def run_pipeline(table_name="sl_test_announcements"):
     # Create a pipeline to load data into Snowflake
     pipeline = dlt.pipeline(
         pipeline_name="extract_from_sl_pipeline", 
@@ -25,7 +49,7 @@ def run_pipeline(table_name="sl_announcements"):
         dataset_name="staging")
     
     # Extract, normalize, and load the data
-    load_info = pipeline.run(sl_announcements_resource(), table_name=table_name)
+    load_info = pipeline.run(sl_announcements_with_coordinates(), table_name=table_name)
     print(load_info)  
 
 if __name__ == "__main__":

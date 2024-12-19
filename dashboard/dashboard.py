1b0ae6882851c3a5ffbf4d055ea219178f688de2
simontroedsson <simon.troedsson94@gmail.com>
2024-12-19 12:17:22 +0100

    update filter code

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -13,34 +13,39 @@ STOCKHOLM_CENTER = (59.325,18.05)
 def layout():
     df = query_traffic_messages().dropna(subset=['LONGITUDE'])
     
-    
-    st.header("Traffic Stockholm")			
+
     map = folium.Map(location=STOCKHOLM_CENTER,zoom_start=9,)
     cols = st.columns([0.12, 0.88],vertical_alignment="top")
     
-    # Filter on means of transport
-    with cols[0]:
-        car_selected = st.checkbox("Car")
-        bus_selected = st.checkbox("Bus")
-        metro_selected = st.checkbox("Metro")
-        train_selected = st.checkbox("Train")
-        tram_selected = st.checkbox("Tram")
-        ferry_selected = st.checkbox("Ferry")
+    traffic_deviation_map = {
+        "Car": ["Vägarbete", "Hastighetsbegränsning gäller", "Vägen avstängd", "Fordonshaveri",
+                "Olycka", "Följ omledningsskyltar", "Körfältsavstängningar", "Bärgning", "Trafiksignal fungerar ej"],
+        "Bus": ["Vägarbete", "Hastighetsbegränsning gäller", "Vägen avstängd", "Fordonshaveri",
+                "Olycka", "Följ omledningsskyltar", "Körfältsavstängningar", "Bärgning", "Trafiksignal fungerar ej"],
+        "Metro": [],  
+        "Train": [],  
+        "Tram": [],  
+        "Ferry": ["Färja"]
+    }
 
-    # Filter dataframe on traffic deviations that are relevant to the means of transport
-    if car_selected or bus_selected:
-        car_traffic_deviation = ["Vägarbete","Hastighetsbegränsning gäller","Vägen avstängd","Fordonshaveri",
-                                 "Olycka","Följ omledningsskyltar","Körfältsavstängningar","Bärgning","Trafiksignal fungerar ej"]
-        df = df.query(f"MESSAGE_CODE in {car_traffic_deviation}")
-    if metro_selected:
-        pass
-    if train_selected:
-        pass
-    if tram_selected:
-        pass
-    if ferry_selected:
-        ferry_traffic_deviations = ["Färja"]
-        df = df.query(f"MESSAGE_CODE in {ferry_traffic_deviations}")
+    selected_deviations = []
+    with cols[0]:
+        if st.checkbox("Car"):
+            selected_deviations.extend(traffic_deviation_map["Car"])
+        if st.checkbox("Bus"):
+            selected_deviations.extend(traffic_deviation_map["Bus"])
+        if st.checkbox("Metro"):
+            selected_deviations.extend(traffic_deviation_map["Metro"])
+        if st.checkbox("Train"):
+            selected_deviations.extend(traffic_deviation_map["Train"])
+        if st.checkbox("Tram"):
+            selected_deviations.extend(traffic_deviation_map["Tram"])
+        if st.checkbox("Ferry"):
+            selected_deviations.extend(traffic_deviation_map["Ferry"])
+
+    # filter dataframe for seleceted deviations
+    if selected_deviations:
+        df = df[df["MESSAGE_CODE"].isin(selected_deviations)]
 
     # Loop over dataframe to place markers on the map for every Traffic deviation
     for index, row in df.iterrows():

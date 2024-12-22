793198f5755ce91549debd85f4231ce9d71cc972
Larissa Dovgoborets <larissa.dovgoborets@outlook.com>
2024-12-22 21:37:07 +0100

    finish fct_sl_announcement.sql

--- pipeline/transform/dbt_code/models/fct/fct_sl_announcement.sql
+++ pipeline/transform/dbt_code/models/fct/fct_sl_announcement.sql
@@ -8,26 +8,30 @@ sl_stp_area AS (SELECT * FROM {{ ref('src_sl_scope_stop_area') }}),
 
 sl_stp_point AS (SELECT * FROM {{ ref('src_sl_scope_stop_point') }}),
 
-sl_stp_detail AS (SELECT * FROM {{ ref('src_sl_stop_point_detail') }}),
+sl_stp_detail AS (SELECT * FROM {{ ref('src_sl_stop_point_detail') }})
 
 
 SELECT 
-    id,
-    {{dbt_utils.generate_surrogate_key(['src_location.location_id','src_location.coordinates'])}} as location_id,
-    {{dbt_utils.generate_surrogate_key(['src_deviation.deviation_id', 'src_deviation.affected_direction'])}} as deviation_id,
-    COALESCE(header,'ej specificerad') AS header,
-    COALESCE(message,'Detaljerad informaion saknas') AS message,
-    message_code,
-    message_code_value,
-    message_type,
-    safety_related_message,
-    start_time,
-    end_time,
-    valid_until_further_notice
+
+    {{dbt_utils.generate_surrogate_key(['sl_ann.deviation_case_id', 'sl_ann.version'])}} as announcement_id,
+    sl_ann.deviation_case_id as deviation_case_id,
+    version,
+    created,
+    publish_from,
+    publish_upto,
+    header,
+    details as message,
+    priority,
+    scope_alias,
+    sl_line.id as line_key,
+    sl_stp_area.id as stop_area_key,
+    sl_stp_point.id as stop_point_key
 
 FROM 
-    fct_traffic_msg 
+    sl_ann
 LEFT JOIN 
-    src_location ON fct_traffic_msg.id = src_location.location_id
+    sl_msg ON sl_ann._dlt_id = sl_msg._dlt_parent_id
 LEFT JOIN 
-    src_deviation ON fct_traffic_msg.id = src_deviation.deviation_id
+    sl_line ON sl_ann._dlt_id = sl_line._dlt_parent_id
+LEFT JOIN sl_stp_area ON sl_ann._dlt_id = sl_stp_area._dlt_parent_id
+LEFT JOIN sl_stp_point ON sl_ann._dlt_id = sl_stp_point._dlt_root_id

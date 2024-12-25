2310d426410e4fd2fd5f36af1ebfd12803c8f9d6
Larissa Dovgoborets <larissa.dovgoborets@outlook.com>
2024-12-25 10:01:01 +0100

    create function to query sl mart data

--- dashboard/connect_data_warehouse.py
+++ dashboard/connect_data_warehouse.py
@@ -23,8 +23,29 @@ def query_traffic_messages(query='SELECT * FROM mart_trafikverket_traffic_messag
         df = pd.read_sql(query, conn)
 
         return df
+
+def query_sl_announcements(query='SELECT * FROM mart_sl_announcements'):
+
+    load_dotenv()
+
+    with snowflake.connector.connect(
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database=os.getenv("SNOWFLAKE_DATABASE"),
+        schema=os.getenv("SNOWFLAKE_SCHEMA"),
+        role=os.getenv("SNOWFLAKE_ROLE"),
+    ) as conn:
+
+        # Execute the query
+        df = pd.read_sql(query, conn)
+
+        return df
     
 if __name__ == "__main__":
     print(query_traffic_messages())
     df = query_traffic_messages()
     print(df.info())
+    sl_df = query_sl_announcements()
+    print(sl_df.info())

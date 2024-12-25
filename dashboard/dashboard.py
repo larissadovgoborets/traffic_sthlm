182d83abdf7f687632c4ce398e26cbe0befb8ddb
simontroedsson <simon.troedsson94@gmail.com>
2024-12-25 20:36:08 +0100

    remove checkbox filter on vehicle, added multiselect filter on deviation

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -26,8 +26,9 @@ def layout():
         )
 
     query = st.text_input("Sök på avvikelser")
+    deviation = ["Vägarbete","Körfältsavstängningar","Olycka","Färja","Hastighetsbegränsning gäller"]
+    selected_modes = st.multiselect('Välj avvikelse', df["MESSAGE_CODE"].unique(), default=deviation)
     col1, col2, col3 = st.columns(3)
-
     count = df.duplicated(subset=['LONGITUDE', 'LATITUDE'], keep=False).sum()
     with col1:
         st.markdown(f"""
@@ -54,45 +55,6 @@ def layout():
             </div>
         """, unsafe_allow_html=True)
     st.write("")
-
-    cols = st.columns([0.13, 0.87],vertical_alignment="top")
-    
-    traffic_deviation_map = {
-        "Car": ["Vägarbete", "Hastighetsbegränsning gäller", "Vägen avstängd", "Fordonshaveri",
-                "Olycka", "Följ omledningsskyltar", "Körfältsavstängningar", "Bärgning", "Trafiksignal fungerar ej"],
-        "Bus": ["Vägarbete", "Vägen avstängd", "Fordonshaveri",
-                "Olycka", "Följ omledningsskyltar", "Körfältsavstängningar", "Bärgning", "Trafiksignal fungerar ej"],
-        "Metro": [],  
-        "Train": [],  
-        "Tram": [],  
-        "Ferry": ["Färja"]
-    }
-    selected_deviations = []
-    with cols[0]:
-        st.markdown("""
-            <div style="padding: 5px; background-color: #27272f; border-radius: 10px; text-align: center;">
-            <pre>Välj Färdmedel</pre>
-            </div>
-        """, unsafe_allow_html=True)
-        st.write("")
-        if st.checkbox("Car"):
-            selected_deviations.extend(traffic_deviation_map["Car"])
-        if st.checkbox("Bus"):
-            selected_deviations.extend(traffic_deviation_map["Bus"])
-        if st.checkbox("Metro"):
-            selected_deviations.extend(traffic_deviation_map["Metro"])
-        if st.checkbox("Train"):
-            selected_deviations.extend(traffic_deviation_map["Train"])
-        if st.checkbox("Tram"):
-            selected_deviations.extend(traffic_deviation_map["Tram"])
-        if st.checkbox("Ferry"):
-            selected_deviations.extend(traffic_deviation_map["Ferry"])
-   
-   
-    # filter dataframe for seleceted deviations
-    if selected_deviations:
-        df = df[df["MESSAGE_CODE"].isin(selected_deviations)]
-
     deviation_map = folium.Map(location=STOCKHOLM_CENTER,zoom_start=9)   
     
     # Loop over dataframe to place markers on the map for every Traffic deviation
@@ -112,12 +74,11 @@ def layout():
                         popup=folium.Popup(html_string,max_width=None)).add_to(deviation_map)
        
     
-    with cols[1]:
-        #st_folium(deviation_map,height=450,use_container_width=True)
-        deviation_map = deviation_map._repr_html_()
-        html(deviation_map,height=600)
-
     
+    st_folium(deviation_map,height=450,use_container_width=True)
+    #deviation_map = deviation_map._repr_html_()
+    #html(deviation_map,height=600)
+
 
 if __name__ == "__main__":
     layout()

28498ad501d58ed611ce89a502c577da8ee38b8d
Larissa Dovgoborets <larissa.dovgoborets@outlook.com>
2025-01-05 21:14:24 +0100

    add sql-files to model to point to dgstr sprecicific tables to avoid schema corruption

---
+++ pipeline/transform/dbt_code/models/marts/mart_trafikverket_traffic_messages_dgstr.sql
@@ -0,0 +1,32 @@
+WITH fct_traffic_msg AS (SELECT * FROM {{ ref('fct_traffic_messages') }}),
+
+src_location AS (SELECT * FROM {{ ref('dim_location') }}),
+
+src_deviation AS (SELECT * FROM {{ ref('dim_traffic_deviation_details') }})
+
+SELECT 
+    id,
+    header,
+    message,
+    message_code,
+    message_type,
+    location_descriptor,
+    longitude,
+    latitude,
+    start_time,
+    end_time,
+    affected_direction,
+    number_of_lanes_restricted,
+    road_number,
+    road_number_numeric,
+    temporary_limit,
+    traffic_restriction_type,
+    road_name,
+    severity_text
+
+FROM 
+    fct_traffic_msg 
+LEFT JOIN 
+    src_location ON fct_traffic_msg.location_id = src_location.location_id
+LEFT JOIN 
+    src_deviation ON fct_traffic_msg.deviation_id = src_deviation.deviation_id

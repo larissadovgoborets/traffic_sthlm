a11f0ec2a7734f67d08763133bc4b20bfa0975f6
Larissa Dovgoborets <larissa.dovgoborets@outlook.com>
2025-01-07 02:02:36 +0100

    Update connection to consume trafikverket data from mart-table created by dagster

--- dashboard/connect_data_warehouse.py
+++ dashboard/connect_data_warehouse.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 @st.cache_data
-def query_traffic_messages(query='SELECT * FROM mart_trafikverket_traffic_messages'):
+def query_traffic_messages(query='SELECT * FROM mart_trafikverket_traffic_messages_dgstr'):
 
     load_dotenv()
 

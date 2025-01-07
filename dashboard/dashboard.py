df2b0b221acc5a2fa817f79d81203e53754eec6e
simontroedsson <simon.troedsson94@gmail.com>
2025-01-07 13:40:05 +0100

    add filter on start time

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,8 +6,8 @@ from connect_data_warehouse import query_traffic_messages
 from folium_map_utility_functions import create_marker_tooltip, get_deviation_icon_image_path
 from streamlit.components.v1 import html
 from present_sl_data import display_sl_data
-
-
+import pandas as pd
+from datetime import datetime
 STOCKHOLM_CENTER = (59.325,18.05)
 
 def layout():
@@ -25,13 +25,19 @@ def layout():
 
     query = st.text_input("Sök på väg")
     deviation_types = df["MESSAGE_CODE"].unique()
+    # Filter on deviation
     selected_types = st.multiselect('Välj avvikelse', deviation_types, default=deviation_types)
     filtered_df = df[df['MESSAGE_CODE'].isin(selected_types)]
     
+    # Filter on start_time
+    selected_date = st.date_input("Select start date")
+    filtered_df = filtered_df[filtered_df["START_TIME"].dt.date == selected_date]
+    
+    #Filter on search query
     if query:
         filtered_df = filtered_df[df["LOCATION_DESCRIPTOR"].str.contains(rf'\b{query}\b', case=False, na=False) |
                                   df["ROAD_NUMBER"].str.contains(rf'\b{query}\b', case=False, na=False)]
-
+    
     deviation_map = folium.Map(location=STOCKHOLM_CENTER,zoom_start=9)   
     # Loop over dataframe to place markers on the map for every Traffic deviation
     for index, row in filtered_df.iterrows():
